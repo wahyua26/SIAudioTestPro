d98856ccf6fe7a496b75117f7906333d409a97c0
Wahyu <ajiwahyu917@gmail.com>
2023-06-08 14:00:37 +0700

    update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('tglLahir');
+            $table->string('jabatan');
+            $table->string('status');
+            $table->string('foto')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
